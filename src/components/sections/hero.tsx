b27575452877acc86a34b81cb73bf5a2e741668c
Paloma Emanuelly <emanuwaveh@gmail.com>
2025-09-30 13:20:25 +0000

    NA VERSÃO DESKTOP, DIMINUA LEVEMENTE O TITULO

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -39,7 +39,7 @@ export default function HeroSection() {
         >
           ZAAK MONTAGENS
         </h2>
-        <h1 className="mt-2 text-4xl md:text-6xl font-bold font-headline leading-tight drop-shadow-lg">
+        <h1 className="mt-2 text-4xl md:text-5xl font-bold font-headline leading-tight drop-shadow-lg">
           Seu móvel montado com <span className="text-fill-image">agilidade e experiência</span> em Salvador, Bahia.
         </h1>
         <p className="mt-6 max-w-3xl mx-auto text-lg md:text-xl font-light text-neutral-200 drop-shadow-md">
