d713ab2b2fe26254adf310733ac0b13d29f80e11
Paloma Emanuelly <emanuwaveh@gmail.com>
2025-09-30 13:33:37 +0000

    a imagem vem primeiro, otária

--- src/components/sections/about-me.tsx
+++ src/components/sections/about-me.tsx
@@ -9,12 +9,6 @@ export default function AboutMeSection() {
   return (
     <TexturedSection className="bg-black">
         <div className="flex flex-col items-center gap-12">
-          <div className="text-center">
-            <h2 className="text-3xl md:text-4xl font-bold font-headline text-white mb-6">Sobre Mim</h2>
-            <p className="mt-4 text-lg text-neutral-300 font-light max-w-3xl mx-auto">
-              Olá! Sou Isaque, montador de móveis com mais de 5 anos de experiência em Salvador. Minha missão é oferecer um serviço que une agilidade e perfeição, garantindo que seus móveis fiquem exatamente como você imaginou.
-            </p>
-          </div>
           <div className="relative h-[450px] w-full max-w-sm mx-auto rounded-lg overflow-hidden shadow-lg">
             {aboutMeImage && (
               <Image
@@ -25,6 +19,12 @@ export default function AboutMeSection() {
                 data-ai-hint={aboutMeImage.imageHint}
               />
             )}
+          </div>
+          <div className="text-center">
+            <h2 className="text-3xl md:text-4xl font-bold font-headline text-white mb-6">Sobre Mim</h2>
+            <p className="mt-4 text-lg text-neutral-300 font-light max-w-3xl mx-auto">
+              Olá! Sou Isaque, montador de móveis com mais de 5 anos de experiência em Salvador. Minha missão é oferecer um serviço que une agilidade e perfeição, garantindo que seus móveis fiquem exatamente como você imaginou.
+            </p>
           </div>
         </div>
     </TexturedSection>
