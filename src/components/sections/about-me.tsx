484c8ca24f4754692accb4a218d31858df6f0113
Paloma Emanuelly <emanuwaveh@gmail.com>
2025-09-30 14:25:09 +0000

    mude a cor para a usada no site (_for element h2_)

--- src/components/sections/about-me.tsx
+++ src/components/sections/about-me.tsx
@@ -5,10 +5,10 @@ export default function AboutMeSection() {
   const aboutMeImage = PlaceHolderImages.find(p => p.id === 'about-me-image');
 
   return (
-    <section className="bg-black">
-      <div className="container mx-auto px-4 py-12 sm:py-16">
+    <section className="bg-black py-12 sm:py-16">
+      <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row items-center gap-12 md:gap-16">
-          <div className="relative h-[350px] w-full max-w-xs md:w-1/3 mx-auto rounded-lg overflow-hidden shadow-lg flex-shrink-0">
+          <div className="relative h-[280px] w-full max-w-[280px] md:w-1/3 mx-auto rounded-lg overflow-hidden shadow-lg flex-shrink-0">
             {aboutMeImage && (
               <Image
                 src={aboutMeImage.imageUrl}
@@ -20,7 +20,7 @@ export default function AboutMeSection() {
             )}
           </div>
           <div className="text-center md:w-2/3">
-            <h2 className="text-3xl md:text-4xl font-bold font-headline text-white mb-6">Sobre Mim</h2>
+            <h2 className="text-3xl md:text-4xl font-bold font-headline text-primary mb-6">Sobre Mim</h2>
             <p className="mt-4 text-lg text-neutral-300 font-light max-w-3xl mx-auto">
               Olá! Sou Isaque, o especialista por trás da Zaak Montagens. Com mais de 5 anos de dedicação, minha paixão é transformar ambientes através de um serviço de montagem que combina precisão, agilidade e respeito pelo seu espaço. Minha missão é entregar um resultado impecável, garantindo a durabilidade e a funcionalidade que você espera.
             </p>
