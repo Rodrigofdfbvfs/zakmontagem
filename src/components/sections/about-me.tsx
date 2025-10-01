2c108b7f5df9a960d77d1c5c3c97ec537f385652
Paloma Emanuelly <emanuwaveh@gmail.com>
2025-10-01 14:50:46 +0000

    na seção sobre mim, substitua a foto atual por; <blockquote class="imgur

--- src/components/sections/about-me.tsx
+++ src/components/sections/about-me.tsx
@@ -14,7 +14,8 @@ export default function AboutMeSection() {
               <Image
                 src={aboutMeImage.imageUrl}
                 alt={aboutMeImage.description}
-                fill
+                width={240}
+                height={240}
                 className="object-cover"
                 data-ai-hint={aboutMeImage.imageHint}
               />
@@ -23,7 +24,7 @@ export default function AboutMeSection() {
           <div className="text-center md:w-3/4">
             <h2 className={cn("text-3xl md:text-4xl font-bold font-headline text-fill-image mb-6")}>Sobre Mim</h2>
             <p className="mt-4 text-lg text-neutral-300 font-light max-w-3xl mx-auto">
-              Olá! Sou Isaque, especialista em montagem de móveis com 7 anos de experiência. Minha paixão é transformar ambientes com precisão e agilidade. Já colaborei com grandes nomes como Magazine Luiza, Casas Bahia, MadeiraMadeira, Baianão Móveis e Insinuante, garantindo sempre um resultado impecável.
+              Olá! Sou Isaque, especialista em montagem de móveis com 7 anos de experiência. Minha paixão é transformar ambientes com precisão e agilidade. Já colaborei com grandes nomes como Magazine Luiza, Casas Bahia, MadeiraMadeira, Lojas Baianão e Insinuante, garantindo sempre um resultado impecável.
             </p>
           </div>
         </div>
