1b6f7dbe2536211567adcb15cd04de3893b22c7b
Paloma Emanuelly <emanuwaveh@gmail.com>
2025-10-10 12:25:46 +0000

    instale o tag manage no site
    <!-- Google tag (gtag.js) -->
    <script asy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
@@ -18,6 +19,17 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700&display=swap" rel="stylesheet" />
+        {/* Google tag (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-10902335859"></Script>
+        <Script id="gtag-init">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-10902335859');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased bg-background text-foreground">
         {children}
